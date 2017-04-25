d39290f078619296b2df162fc807af7e1ce3acf6
Denis Tregub <gamager.denis@rambler.ru>
2017-04-25 22:50:13 +0300

    Я поработал как ты и просил

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main() 
 {
+	int a;
 	cout << "Hello World!!!" << endl;
+
+	cout << "Privet Denis, How are you?" << endl;
+
+	cout << "Скажи чколько тебе годков?" << endl;
+	cin >> (int)a;
+
+	if(a <= 15)
+	{
+		cout << "Что-то ты совсем маловат" << endl;
+	}
+	else
+		cout << "Добро пожаловать в команду" << endl;
 
 	system("pause");
 	return 0;
